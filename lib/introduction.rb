019ec3f18cb8ed65121f926bfc616e0c269f230b
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-05 23:44:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,6 +8,13 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  
+def introduction_with_language(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
 
 
 
